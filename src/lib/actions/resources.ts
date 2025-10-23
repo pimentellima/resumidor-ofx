84fd687af427ff452a9b34063d10585e245bf6d2
pimentellima <matheus_dere@hotmail.com>
2025-10-22 21:53:15 -0300

    feat: implement user authentication and resource creation logic

--- src/lib/actions/resources.ts
+++ src/lib/actions/resources.ts
@@ -2,16 +2,19 @@
 
 import { db } from '@lib/db/index'
 import { generateEmbeddings } from '../ai/embedding'
+import { auth } from '../auth'
 import { embeddings as embeddingsTable, resources } from '../db/schema'
 
 export const createResource = async (content: string) => {
     try {
+        const session = await auth()
+        if (!session?.user) throw new Error('Unauthorized')
         if (typeof content !== 'string')
             throw new Error('Content must be a string.')
 
         const [resource] = await db
             .insert(resources)
-            .values({ content })
+            .values({ content, userId: session.user.id })
             .returning()
 
         const embeddings = await generateEmbeddings(content)
--- src/lib/services/users.ts
+++ src/lib/services/users.ts
@@ -1,12 +1,11 @@
 import { db } from '@/lib/db'
 import { users } from '@/lib/db/schema'
 import { eq } from 'drizzle-orm'
-import { cache } from 'react'
 
-export const getUserById = cache(async (userId: string) => {
+export const getUserById = async (userId: string) => {
     const user = await db.query.users.findFirst({
         where: eq(users.id, userId),
     })
     if (!user) throw new Error('User not found in db')
     return user
-})
+}
